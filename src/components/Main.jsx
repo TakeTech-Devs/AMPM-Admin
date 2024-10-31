15672e14e6a70696a916ee330cbd2f962822c01d
Kaustav Ghosh <kaustavghosh444@gmail.com>
2024-11-01 00:43:38 +0530

    reload problem resolve

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -17,30 +17,32 @@ function Main() {
   const location = useLocation();
   const isLoginRoute = location.pathname === "/login";
 
-  const { isAuthenticated} = useSelector((state) => state.adminLogin);
+  const { isAuthenticated } = useSelector((state) => state.adminLogin);
 
-  if (isAuthenticated && isLoginRoute) {
-    return <Navigate to="/" />;
-  }
+  // if (isAuthenticated && isLoginRoute) {
+  //   return <Navigate to="/" />;
+  // }
 
   // Redirect to login if not authenticated and not on login page
-  if (!isAuthenticated && !isLoginRoute) {
-    return <Navigate to="/login" />;
-  }
-  
+  // if (!isAuthenticated && !isLoginRoute) {
+  //   return <Navigate to="/login" />;
+  // }
+
 
   return (
     <div className={`main ${isLoginRoute ? "login-main-show" : ""}`}>
       <div className={`main-page ${isLoginRoute ? "main-page-show" : ""}`}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <Login />} />
+          <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
           <Route path="/aboutus" element={<Aboutus />} />
           <Route path="/product" element={<Product />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/consumers" element={<Users />} />
           <Route path="/resellers" element={<Resellers />} />
           <Route path="/orders" element={<Orders />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/admins" element={<Admins />} />
         </Routes>
       </div>
