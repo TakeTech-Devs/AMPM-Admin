b4c8b0d53ddccae6e1a8ac0f6237ba1671ac7252
Kaustav Ghosh <kaustavghosh444@gmail.com>
2024-11-20 17:45:55 +0530

    some changes 2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Dashboard.scss";
 import "../styles/Global.scss";
 import companyLogo from "../assets/company-logo.png";
 import { NavLink, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { logout } from "../Actions/AdminAction";
-import HomeIcon from '@mui/icons-material/Home';
-import InfoIcon from '@mui/icons-material/Info';
-import Inventory2Icon from '@mui/icons-material/Inventory2';
-import LocalOfferIcon from '@mui/icons-material/LocalOffer';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
-import LogoutIcon from '@mui/icons-material/Logout';
-
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import LogoutIcon from "@mui/icons-material/Logout";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import TocIcon from '@mui/icons-material/Toc';
 
 function Sidebar() {
+  const [showContentMenu, setShowContentMenu] = useState(false);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -25,105 +27,163 @@ function Sidebar() {
   const handleLogout = () => {
     dispatch(logout());
     window.alert("Logged Out Successfully");
-    // navigate("/login");
     window.location.href = "/login";
-
   };
 
   return (
-    <>
-      <div className="sidebar">
-        <div className="logo">
-          <NavLink to="/">
-            <img src={companyLogo} alt="Company Logo" />
-          </NavLink>
-        </div>
-        <div className="menu">
-          <ul>
-            <NavLink to="/" className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link"}>
-              <li className="d-flex align-items-center">
-                <div className="icon1">
-                  <HomeIcon style={{ fontSize: 25, color: "#47ad01" }} />
-                </div>
-                Home
-              </li>
-            </NavLink>
-            <NavLink to="/aboutus" className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link"}>
-              <li className="d-flex align-items-center">
-                <div className="icon1">
-                  <InfoIcon style={{ fontSize: 25, color: "#47ad01" }} />
-                </div>
-                About Us
-              </li>
-            </NavLink>
-            {/* <NavLink to="/product" className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link"}>
-              <li>
-                <div className="icon1 d-flex align-items-center justify-content-center">
-                  <Inventory2Icon style={{ fontSize: 25, color: "#47ad01" }} />
-                </div>
-                Product
-              </li>
-            </NavLink> */}
-            <NavLink to="/contact" className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link"}>
-              <li>
-                <div className="icon1 d-flex align-items-center justify-content-center">
-                  <AccountBoxIcon style={{ fontSize: 25, color: "#47ad01" }} />
-                </div>
-                Contact
-              </li>
-            </NavLink>
-            {/* <NavLink to="/discount" className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link"}>
-              <li>
-                <div className="icon1 d-flex align-items-center justify-content-center">
-                  <LocalOfferIcon style={{ fontSize: 25, color: "#47ad01" }} />
-                </div>
-                Discount
-              </li>
-            </NavLink> */}
-            <NavLink to="/consumers" className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link"}>
-              <li>
-                <div className="icon1 d-flex align-items-center justify-content-center">
-                  <SupervisorAccountIcon style={{ fontSize: 25, color: "#47ad01" }} />
-                </div>
-                Consumers
-              </li>
-            </NavLink>
-            <NavLink to="/resellers" className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link"}>
-              <li>
-                <div className="icon1 d-flex align-items-center justify-content-center">
-                  <StorefrontIcon style={{ fontSize: 25, color: "#47ad01" }} />
-                </div>
-                Resellers
-              </li>
-            </NavLink>
-            <NavLink to="/orders" className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link"}>
-              <li>
-                <div className="icon1 d-flex align-items-center justify-content-center">
-                  <ShoppingCartIcon style={{ fontSize: 25, color: "#47ad01" }} />
-                </div>
-                Orders
-              </li>
-            </NavLink>
-            <NavLink to="/admins" className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link"}>
-              <li>
-                <div className="icon1 d-flex align-items-center justify-content-center">
-                  <AdminPanelSettingsIcon style={{ fontSize: 25, color: "#47ad01" }} />
-                </div>
-                Admins
-              </li>
-            </NavLink>
-            <NavLink onClick={handleLogout} eventKey="link-event-key" className="sidebar-link logout">
-              <li>
-                <div className="icon1 d-flex align-items-center justify-content-center">
-                  <LogoutIcon style={{ fontSize: 25, color: "#47ad01" }} />
-                </div>
-                Logout
-              </li>
-            </NavLink>
-          </ul>
-        </div>
+    <div className="sidebar">
+      <div className="logo">
+        <NavLink to="/">
+          <img src={companyLogo} alt="Company Logo" />
+        </NavLink>
       </div>
-    </>
+      <div className="menu">
+        <ul>
+          {/* Content Management Dropdown */}
+          <li
+            className={({ isActive }) =>
+              isActive ? "sidebar-link active" : "sidebar-link"}
+            onClick={() => setShowContentMenu(!showContentMenu)}
+            style={{ cursor: "pointer", backgroundColor: "#fff", width: "90%", height: "40px", borderRadius: "5px", paddingLeft: "20px", paddingBlock: "22px" }}
+          >
+            <div className="d-flex align-items-center">
+              <TocIcon style={{ fontSize: 25, color: "#47ad01" }} />
+              <span style={{paddingLeft:"10px"}}>Content Management</span>
+              {showContentMenu ? (
+                <ExpandLessIcon style={{ marginLeft: "auto" }} />
+              ) : (
+                <ExpandMoreIcon style={{ marginLeft: "auto" }} />
+              )}
+            </div>
+          </li>
+          {showContentMenu && (
+            <ul className="submenu">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "sidebar-link active" : "sidebar-link"
+                }
+              >
+                <li className="d-flex align-items-center">
+                  <div className="icon1">
+                    <HomeIcon style={{ fontSize: 25, color: "#47ad01" }} />
+                  </div>
+                  Home
+                </li>
+              </NavLink>
+              <NavLink
+                to="/aboutus"
+                className={({ isActive }) =>
+                  isActive ? "sidebar-link active" : "sidebar-link"
+                }
+              >
+                <li className="d-flex align-items-center">
+                  <div className="icon1">
+                    <InfoIcon style={{ fontSize: 25, color: "#47ad01" }} />
+                  </div>
+                  About Us
+                </li>
+              </NavLink>
+              <NavLink
+                to="/contact"
+                className={({ isActive }) =>
+                  isActive ? "sidebar-link active" : "sidebar-link"
+                }
+              >
+                <li className="d-flex align-items-center">
+                  <div className="icon1">
+                    <AccountBoxIcon style={{ fontSize: 25, color: "#47ad01" }} />
+                  </div>
+                  Contact
+                </li>
+              </NavLink>
+              <NavLink
+                to="/discount"
+                className={({ isActive }) =>
+                  isActive ? "sidebar-link active" : "sidebar-link"
+                }
+              >
+                <li className="d-flex align-items-center">
+                  <div className="icon1">
+                    <LocalOfferIcon style={{ fontSize: 25, color: "#47ad01" }} />
+                  </div>
+                  Discount
+                </li>
+              </NavLink>
+            </ul>
+          )}
+          {/* Other Links */}
+          <NavLink
+            to="/consumers"
+            className={({ isActive }) =>
+              isActive ? "sidebar-link active" : "sidebar-link"
+            }
+          >
+            <li className="d-flex align-items-center">
+              <div className="icon1">
+                <SupervisorAccountIcon
+                  style={{ fontSize: 25, color: "#47ad01" }}
+                />
+              </div>
+              Consumers
+            </li>
+          </NavLink>
+          <NavLink
+            to="/resellers"
+            className={({ isActive }) =>
+              isActive ? "sidebar-link active" : "sidebar-link"
+            }
+          >
+            <li className="d-flex align-items-center">
+              <div className="icon1">
+                <StorefrontIcon style={{ fontSize: 25, color: "#47ad01" }} />
+              </div>
+              Resellers
+            </li>
+          </NavLink>
+          <NavLink
+            to="/orders"
+            className={({ isActive }) =>
+              isActive ? "sidebar-link active" : "sidebar-link"
+            }
+          >
+            <li className="d-flex align-items-center">
+              <div className="icon1">
+                <ShoppingCartIcon style={{ fontSize: 25, color: "#47ad01" }} />
+              </div>
+              Orders
+            </li>
+          </NavLink>
+          <NavLink
+            to="/admins"
+            className={({ isActive }) =>
+              isActive ? "sidebar-link active" : "sidebar-link"
+            }
+          >
+            <li className="d-flex align-items-center">
+              <div className="icon1">
+                <AdminPanelSettingsIcon
+                  style={{ fontSize: 25, color: "#47ad01" }}
+                />
+              </div>
+              Admins
+            </li>
+          </NavLink>
+          <NavLink
+            onClick={handleLogout}
+            eventKey="link-event-key"
+            className="sidebar-link logout"
+          >
+            <li className="d-flex align-items-center">
+              <div className="icon1">
+                <LogoutIcon style={{ fontSize: 25, color: "#47ad01" }} />
+              </div>
+              Logout
+            </li>
+          </NavLink>
+        </ul>
+      </div>
+    </div>
   );
 }
 
--- src/pages/Discount.jsx
+++ src/pages/Discount.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Form, Container, Row, Col, Button, Table } from 'react-bootstrap';
+import '../styles/Dashboard.scss';
+import '../styles/Global.scss';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 const Discount = () => {
-  return (
-    <div>Discount</div>
-  )
+    return (
+        <div className="cardbox">
+            <h3>Discounts</h3>
+            <Container>
+                <Row>
+                    <Form>
+                        <Col lg={12}>
+                            <Form.Control
+                                type="text"
+                                placeholder="Discount Number"
+                                
+                            />
+                        </Col>
+                        <Col lg={12} className="d-flex align-items-center">
+                            <Form.Control
+                                type="text"
+                                placeholder="Discount Code"
+                            />
+                            <Button variant="outline-success" className="ms-2">
+                                <AddIcon style={{ fontSize: "20px" }} />
+                            </Button>
+                        </Col>
+                        <Col lg={12} className="mt-2">
+                            
+                                <div
+                                    // key={i}
+                                    className="d-flex justify-content-between align-items-center p-2 mb-2 border rounded bg-light">
+                                    <span className="text"></span>
+                                    <Button className="d-flex justify-content-center btn-highlight" variant="outline-danger">
+                                        <DeleteForeverIcon style={{ fontSize: "20px" }} />
+                                    </Button>
+                                </div>
+                            
+                        </Col>
+                        <Col xs={12} className="d-flex justify-content-end gap-2 mt-2">
+                            <Button variant="danger" >
+                                Clear
+                            </Button>
+                            <Button variant="success">
+                                Submit
+                            </Button>
+                        </Col>
+                    </Form>
+                </Row>
+                <Table bordered hover responsive>
+                    <thead>
+                        <tr>
+                            <th>Discount Number</th>
+                            <th>Discount Code</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td style={{ width: "600px" }}>1</td>
+                            <td>
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        alignItems: "center",
+                                        justifyContent: "space-between",
+                                        marginBottom: "8px",
+                                    }}
+                                >
+                                    <span style={{ marginBottom: "20px" }}></span>
+                                    <Button
+                                        variant="outline-danger"
+                                        className='d-flex align-items-center justify-content-center'
+                                        style={{ padding: "4px", minWidth: "32px" }}
+                                    >
+                                        <DeleteForeverIcon style={{ fontSize: "20px" }} />
+                                    </Button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Container>
+        </div>
+
+    )
 }
 
 export default Discount
